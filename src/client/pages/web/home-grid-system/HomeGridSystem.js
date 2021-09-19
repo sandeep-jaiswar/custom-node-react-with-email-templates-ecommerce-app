3bb6bfd5863c29bbb87592124c1f1ab7293da4ee
sandeepjaiswar <sandeep.jaiswar3@justdial.com>
2021-09-19 18:12:12 +0530

    styles for arrow on slider btn

--- src/client/pages/web/home-grid-system/HomeGridSystem.js
+++ src/client/pages/web/home-grid-system/HomeGridSystem.js
@@ -48,15 +48,40 @@ function HomeGridSystem() {
         }
         return array;
     }
+
+    const getSLiderArrowBtn = key => {
+        const left = key === 'left' ? 1 : 96;
+        const getSvg = key => {
+            if (key === 'left') {
+                return (<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-chevron-left leftArrow" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
+                </svg>)
+            }
+            return (
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-chevron-right rightArrow" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
+                </svg>
+            )
+        }
+        return (
+            <div className='svg-circle' style={{ left: `${left}%` }}>
+                <div className="svg-wrapper">
+                    {getSvg(key)}
+                </div>
+            </div>
+        )
+    }
     const getSliderCarousel = arr => {
         return (
             <div className='slider-carousel'>
+                {getSLiderArrowBtn('left')}
                 {arr.map((cur, i) => (
-                    <div className='' key={i}>
+                    <div className='slider-carousel-content' key={i}>
                         <Image url={cur.url} customImgClass={`slider-carousel-img`} />
-                        {cur.name && <div>{cur.name}</div>}
+                        {cur.name && <div className='slider-carousel-footerText'>{cur.name}</div>}
                     </div>
                 ))}
+                {getSLiderArrowBtn('right')}
             </div>
         )
     }
